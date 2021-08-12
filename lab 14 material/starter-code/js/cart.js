7db5b0e9fe22a0ce5af72d7db644a9d1e41ab886
Demarcus Lester <demarcuslester12@gmail.com>
2021-08-12 18:07:28 -0500

    Demarcus worked on remove item functions event with Spencer guiding me. We implmented as far as getting the new deleted chart displaying, but not removing the old chart

--- lab 14 material/starter-code/js/cart.js
+++ lab 14 material/starter-code/js/cart.js
@@ -9,6 +9,7 @@ let cart;
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
   cart = new Cart(cartItems);
+
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
@@ -19,31 +20,59 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  const tableBody = document.querySelector('#cart tbody');
+
+  for (let index = 0; index < cart.items.length; index++) {
+
+
+    let chartRow = document.getElementById('chartRow');
+
+    if (chartRow != null) {
+      console.log('This is a null row!');
+
+      if (chartRow[index] === true) {
+
+        // tableBody.remove(chartRow[index]);
+        chartRow.remove();
+        console.log('removing');
+
+      }
+    }
+
+  }
+
+
+};
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-
+  // console.log(' Show cart sanity')
   // TODO: Find the table body
-const tableBody = document.querySelector('#cart tbody');
+  const tableBody = document.querySelector('#cart tbody');
   // TODO: Iterate over the items in the cart
-  for (let i = 0; i < cart.length; i++) {
-   let newRow = document.createElement('tr');
-   let buttonCell = document.createElement('td');
-   let deleteButton = document.createElement('button');
-   deleteButton.innerText = 'X';
-   buttonCell.appendChild(deleteButton);
-   let quantityCell = document.createElement('td');
-   quantityCell.innerText = cart[i].quantity;
-   let productNameCell = document.createElement('td');
-   productNameCell.innerText = cart[i].product;
-   newRow.appendChild(buttonCell);
-   newRow.appendChild(quantityCell);
-   newRow.appendChild(productNameCell)
-  
-    
+  for (let i = 0; i < cart.items.length; i++) {
+    console.log(cart.items);
+    let newRow = document.createElement('tr');
+    newRow.classList.add('chartRow');
+    let buttonCell = document.createElement('td');
+    let deleteButton = document.createElement('button');
+    deleteButton.id = i;
+    deleteButton.classList.add('delete');
+    deleteButton.innerText = 'X';
+    buttonCell.appendChild(deleteButton);
+    let quantityCell = document.createElement('td');
+    quantityCell.innerText = cart.items[i].quantity;
+    let productNameCell = document.createElement('td');
+    productNameCell.innerText = cart.items[i].product;
+    newRow.appendChild(buttonCell);
+    newRow.appendChild(quantityCell);
+    newRow.appendChild(productNameCell);
+
+    tableBody.appendChild(newRow);
+
   }
-  tableBody.appendChild(newRow);
+  // tableBody.appendChild(newRow);
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
@@ -51,12 +80,24 @@ const tableBody = document.querySelector('#cart tbody');
 }
 
 function removeItemFromCart(event) {
+  event.preventDefault();
+  console.log(cart);
+  if (event.target.classList.contains('delete')) {
+    // console.log(event.target);
+    // console.log(event.target.id);
+    cart.removeItem(event.target.id);
+    console.log(cart);
+    // Cart.saveToLocalStorage();
+    cart.saveToLocalStorage();
+    renderCart();
+  }
+
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
 
-}
+};
 
 // This will initialize the page and draw the cart on screen
 renderCart();
